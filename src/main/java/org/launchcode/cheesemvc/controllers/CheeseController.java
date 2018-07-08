b2e9d52402051bb7f83f69709690b91ec594d7f1
Filip Swat <filipaswat@gmail.com>
2018-07-08 12:00:46 -0500

    added removal function, validated add function

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
+import java.util.regex.Pattern;
 import java.util.HashMap;
 
 @Controller
@@ -31,8 +31,32 @@ public class CheeseController {
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String processAddCheeseForm(@RequestParam String cheeseName, @RequestParam String cheeseDesc) {
-        cheeses.put(cheeseName, cheeseDesc);
+    public String processAddCheeseForm(@RequestParam String cheeseName, @RequestParam String cheeseDesc, Model model) {
+        if (Pattern.matches("[a-zA-z]([a-zA-z\\s])*", cheeseName)) {
+            cheeses.put(cheeseName, cheeseDesc);
+            return "redirect:";
+        } else {
+            model.addAttribute("errorMessage", "Name must start with a letter and can only contain letters and spaces.");
+            return "cheese/add";
+        }
+    }
+
+    @RequestMapping(value = "remove", method = RequestMethod.GET)
+    public String displayRemoveCheeseForm(Model model) {
+        model.addAttribute("cheeses", cheeses);
+        return "cheese/remove";
+    }
+
+    @RequestMapping(value ="remove", method = RequestMethod.POST)
+    public String processRemoveCheeseForm(HttpServletRequest request) {
+        String[] cheeseName = request.getParameterValues("cheeseName");
+
+        if (cheeseName != null) {
+            for (String cheese : cheeseName) {
+                cheeses.remove(cheese);
+            }
+        }
+
         return "redirect:";
     }
 }
