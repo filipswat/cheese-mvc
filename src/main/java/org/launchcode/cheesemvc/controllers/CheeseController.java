6ec38a2ab48e4b20c30ec7f724ae94135dea418c
Filip Swat <filipaswat@gmail.com>
2018-07-08 23:25:08 -0500

    refactored view / controller for new Cheese class

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -1,12 +1,14 @@
 package org.launchcode.cheesemvc.controllers;
 
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
+import org.launchcode.cheesemvc.models.Cheese;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
 import java.util.regex.Pattern;
 import java.util.HashMap;
 
@@ -14,10 +16,9 @@ import java.util.HashMap;
 @RequestMapping("cheese")
 public class CheeseController {
 
-    static HashMap<String, String> cheeses = new HashMap<>();
+    private static ArrayList<Cheese> cheeses = new ArrayList<>();
 
     @RequestMapping(value = "")
-
     public String index(Model model) {
         model.addAttribute("cheeses", cheeses);
         model.addAttribute("title", "My Cheeses");
@@ -33,7 +34,9 @@ public class CheeseController {
     @RequestMapping(value = "add", method = RequestMethod.POST)
     public String processAddCheeseForm(@RequestParam String cheeseName, @RequestParam String cheeseDesc, Model model) {
         if (Pattern.matches("[a-zA-z]([a-zA-z\\s])*", cheeseName)) {
-            cheeses.put(cheeseName, cheeseDesc);
+            Cheese cheese = new Cheese(cheeseName, cheeseDesc);
+            // benefit of hashmap was that it ensured two cheeses wouldn't share a name
+            cheeses.add(cheese);
             return "redirect:";
         } else {
             model.addAttribute("errorMessage", "Name must start with a letter and can only contain letters and spaces.");
@@ -49,11 +52,19 @@ public class CheeseController {
 
     @RequestMapping(value ="remove", method = RequestMethod.POST)
     public String processRemoveCheeseForm(HttpServletRequest request) {
-        String[] cheeseName = request.getParameterValues("cheeseName");
+        String[] cheeseNames = request.getParameterValues("cheeseNames");
 
-        if (cheeseName != null) {
-            for (String cheese : cheeseName) {
-                cheeses.remove(cheese);
+        if (cheeseNames != null) {
+            for (String cheeseName : cheeseNames) {
+                for (Cheese cheese : cheeses) {
+                    if (cheese.getName().equals(cheeseName)) {
+                        cheeses.remove(cheese);
+                        // could remove the break to make sure all cheeses names cheeseName get deleted
+                        // could add second step comparing description to be deleted to ensure the righ tone is deleted
+                        // neither option seems worth the effort
+                        break;
+                    }
+                }
             }
         }
 
